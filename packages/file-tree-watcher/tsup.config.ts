eeeaef770112df9beb0d8af8965e8c7f358e6162
Juer.G Whang <juergenie@outlook.com>
2024-12-13 02:41:06 +0800

    fix: `dynamic require` issue. updated: `@chrock-studio/file-tree-watcher@0.1.2`, `@chrock-studio/unplugin-auto-export@0.1.2`.

--- packages/file-tree-watcher/tsup.config.ts
+++ packages/file-tree-watcher/tsup.config.ts
@@ -7,4 +7,5 @@ export default defineConfig({
   dts: true,
   outDir: "dist",
   clean: true,
+  target: "node20",
 });
--- packages/unplugin-auto-export/tsup.config.ts
+++ packages/unplugin-auto-export/tsup.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   config: "./tsconfig.json",
-  entry: [
-    "lib/index.ts",
-    "lib/vite.ts",
-    "lib/webpack.ts",
-    "lib/core/formatter.ts",
-  ],
+  entry: ["lib/index.ts", "lib/vite.ts", "lib/webpack.ts", "lib/core/formatter.ts"],
   format: "esm",
   dts: true,
   outDir: "dist",
   clean: true,
+  target: "node20",
 });
